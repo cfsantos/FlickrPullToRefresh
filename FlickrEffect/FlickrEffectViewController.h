f5c2d00d118ebc8c9657cfffc6f2a9aff6b8c5fd
Claudio Santos <claudio.goncalves@abril.com.br>
2014-08-05 14:03:20 -0300

    Animation done. need to work on zPostion

--- FlickrEffect/FlickrEffectViewController.h
+++ FlickrEffect/FlickrEffectViewController.h
@@ -12,12 +12,26 @@
 
 #define REFRESH_HEADER_HEIGHT 52.0f
 
+#define MINORSCALE 0.25
+#define MAJORSCALE 1/MINORSCALE
+#define LEFTLIMIT 140
+#define RIGHTLIMIT 190
+#define CIRCLECENTERX 160
+#define CIRCLECENTERY 100
+
+
 @property(nonatomic, weak)IBOutlet UIScrollView *myScrolView;
 @property(nonatomic, strong)UIImageView *myImageView;
+@property(nonatomic, strong)UIImageView *faceImageView;
+
 @property(nonatomic, strong)UIView *underCircle;
+@property(nonatomic, strong)UIView *pinkCircle;
 
 @property(nonatomic)BOOL isDragging;
 @property(nonatomic)BOOL isLoading;
 @property(nonatomic)BOOL isAnimating;
+@property(nonatomic)BOOL shouldAnimate;
+
+
 
 @end
--- FlickrEffect/FlickrEffectViewController.m
+++ FlickrEffect/FlickrEffectViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.shouldAnimate = YES;
     self.isAnimating = FALSE;
     self.myScrolView.delegate = self;
     self.myScrolView.contentSize = CGSizeMake(320, 4000);
@@ -25,6 +26,9 @@
     self.myImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"garden.jpg"]];
     self.myImageView.frame = CGRectMake(0, 0, 320, 200);
     [self.myScrolView addSubview:self.myImageView];
+    
+    self.pinkCircle = [[UIView alloc] initWithFrame:CGRectMake(110, 50, 100, 100)];
+
     
     self.underCircle = [[UIView alloc] initWithFrame:CGRectMake(110, 50, 100, 100)];
     //self.underCircle = [[UIView alloc] init];
@@ -39,8 +43,12 @@
     shape.path = path.CGPath;
     self.underCircle.layer.mask = shape;
     
+    self.pinkCircle.layer.mask = shape;
+    self.pinkCircle.backgroundColor = [UIColor clearColor];
     self.underCircle.backgroundColor = [UIColor clearColor];
+    
     [self.myImageView addSubview:self.underCircle];
+    [self.myImageView addSubview:self.pinkCircle];
 }
 
 - (void)didReceiveMemoryWarning
@@ -62,8 +70,10 @@
 
         self.myImageView.transform = scale;
         self.myImageView.frame = CGRectMake(self.myImageView.frame.origin.x, scrollOffset, self.myImageView.frame.size.width, self.myImageView.frame.size.height);
+        if (self.shouldAnimate) {
+            [self drawUnderCircleForOffset:y];
+        }
         
-        [self drawUnderCircleForOffset:y];
     }
     
     if (y > REFRESH_HEADER_HEIGHT) {
@@ -75,13 +85,64 @@
     if (self.isAnimating)
         return;
     
+
+    [self.underCircle addSubview:self.faceImageView];
     self.isAnimating = TRUE;
-    [UIView animateWithDuration:2 animations:^{
-        self.underCircle.layer.position = CGPointMake(250, 100);
-    } completion:^(BOOL finished){
-        self.isAnimating = FALSE;
-        self.underCircle.layer.position = CGPointMake(160, 100);
+    self.pinkCircle.backgroundColor = [UIColor purpleColor];
+    
+    /*CABasicAnimation *rotation;
+    rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    rotation.fromValue = [NSValue valueWithCGPoint:CGPointMake(160, 100)];
+    rotation.toValue = [NSValue valueWithCGPoint:CGPointMake(260, 100)];
+    rotation.duration = 1.1; // Speed
+    rotation.repeatCount = HUGE_VALF; // Repeat forever. Can be a finite number.
+    [self.underCircle.layer addAnimation:rotation forKey:@"Spin"];*/
+    
+    self.pinkCircle.layer.zPosition = -1;
+    
+    [UIView animateWithDuration:1.0f animations:^{
+        
+        self.pinkCircle.layer.position = CGPointMake(CIRCLECENTERX, CIRCLECENTERY);
+        self.pinkCircle.layer.position = CGPointMake(LEFTLIMIT, CIRCLECENTERY);
+        
+        
+        self.underCircle.layer.position = CGPointMake(CIRCLECENTERX, CIRCLECENTERY);
+        self.underCircle.layer.position = CGPointMake(RIGHTLIMIT, CIRCLECENTERY);
+        
+        
+        CGAffineTransform transform = self.pinkCircle.transform;
+        transform = CGAffineTransformScale(transform, MINORSCALE, MINORSCALE);
+        self.pinkCircle.transform = transform;
+        self.underCircle.transform = transform;
+    }completion:^(BOOL finished){
+        [UIView animateWithDuration:1.0f  delay:0 options:( UIViewAnimationOptionRepeat |UIViewAnimationOptionAutoreverse ) animations:^{
+            self.pinkCircle.layer.position = CGPointMake(LEFTLIMIT, CIRCLECENTERY);
+            self.pinkCircle.layer.position = CGPointMake(RIGHTLIMIT, CIRCLECENTERY);
+            self.underCircle.layer.zPosition = -2;
+            self.pinkCircle.layer.zPosition = 2;
+            
+            self.underCircle.layer.position = CGPointMake(RIGHTLIMIT, CIRCLECENTERY);
+            self.underCircle.layer.position = CGPointMake(LEFTLIMIT, CIRCLECENTERY);
+            self.underCircle.layer.zPosition = 2;
+            self.pinkCircle.layer.zPosition = -2;
+            
+            
+        } completion:^(BOOL finished){
+            self.isAnimating = FALSE;
+            self.underCircle.layer.zPosition = -999;
+            /*NSTimeInterval duration = 1 - abs(self.pinkCircle.layer.frame.origin.x/160);
+            
+            [UIView animateWithDuration:duration animations:^{
+                self.underCircle.layer.position = CGPointMake(160, 100);
+                self.pinkCircle.layer.position = CGPointMake(160, 100);
+            }];*/
+            
+            //self.underCircle.layer.position = CGPointMake(160, 100);
+        }];
     }];
+    
+    
+    
     
 }
 
@@ -173,9 +234,6 @@
                 faceImage.frame = CGRectMake(120, 50, 80, 100);
                 
                 
-                /*UIView *view = [[UIView alloc] initWithFrame:CGRectMake(120, 350, 200, 200)];
-                view.backgroundColor = [UIColor blueColor];*/
-                
                 CAShapeLayer *shape = [CAShapeLayer layer];
                 UIBezierPath *path;
                 
@@ -187,16 +245,15 @@
                 shape.path = path.CGPath;
                 faceImage.layer.mask = shape;
                 
-
+                self.faceImageView = faceImage;
                 
-                /*CAShapeLayer *shape = [CAShapeLayer layer];
-                UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:faceImage.center radius:(2*faceImage.bounds.size.width) startAngle:0 endAngle:(2 * M_PI) clockwise:YES];
-                shape.path = path.CGPath;
-                faceImage.layer.mask=shape;*/
+                UIGraphicsBeginImageContext(self.pinkCircle.frame.size);
+                [image drawInRect:self.pinkCircle.bounds];
+                image = UIGraphicsGetImageFromCurrentImageContext();
+                UIGraphicsEndImageContext();
                 
-                [self.myImageView addSubview:faceImage];
-                [self performSelector:@selector(stopLoading) withObject:nil afterDelay:3.0];
-                //[self stopLoading];
+                self.pinkCircle.backgroundColor = [UIColor colorWithPatternImage:image];
+                [self performSelector:@selector(stopLoading) withObject:nil afterDelay:1.5];
             });
         }
     }] resume];
@@ -205,9 +262,46 @@
 -(void)stopLoading{
     self.isLoading = NO;
     
-    [UIView animateWithDuration:0.3 animations:^{
-        self.myScrolView.contentInset = UIEdgeInsetsZero;
+    NSLog(@"self.underCircle.layer.position = %@ ",NSStringFromCGPoint([self.underCircle.layer.presentationLayer position]));
+    
+    CGPoint underCirclePoint = [self.underCircle.layer.presentationLayer position];
+    CGPoint pinkCirclePoint = [self.pinkCircle.layer.presentationLayer position];
+    
+    self.pinkCircle.layer.position = pinkCirclePoint;
+    self.underCircle.layer.position = underCirclePoint;
+    
+    [UIView animateWithDuration:1.0f animations:^{
+        CGAffineTransform pinkTransform = self.pinkCircle.transform;
+        CGAffineTransform underTransform = self.underCircle.transform;
+        
+        self.pinkCircle.layer.position = CGPointMake(CIRCLECENTERX, CIRCLECENTERY);
+        self.underCircle.layer.position = CGPointMake(CIRCLECENTERX, CIRCLECENTERY);
+        
+        pinkTransform = CGAffineTransformScale(pinkTransform, MAJORSCALE , MAJORSCALE);
+        self.pinkCircle.transform = pinkTransform;
+        
+        
+        underTransform = CGAffineTransformScale(underTransform, MAJORSCALE , MAJORSCALE);
+        self.underCircle.transform = underTransform;
+        
+    } completion:^(BOOL finished){
+        [self.underCircle.layer removeAllAnimations];
+        [self.pinkCircle.layer removeAllAnimations];
     }];
+    
+    
+    self.shouldAnimate = NO;
+    [UIView animateWithDuration:1 animations:^{
+        //self.myScrolView.contentInset = UIEdgeInsetsMake(REFRESH_HEADER_HEIGHT, 0, 0, 0);
+        self.myScrolView.contentInset = UIEdgeInsetsZero;
+    } completion:^(BOOL finished){
+        self.shouldAnimate = YES;
+        //self.pinkCircle.backgroundColor = [UIColor clearColor];
+        self.underCircle.backgroundColor = [UIColor clearColor];
+        //self.myScrolView.contentInset = UIEdgeInsetsZero;
+    }];
+    
+    
 }
 
 
